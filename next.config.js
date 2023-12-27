500ed9b00186854839affc6fdab473e8ba3d6a68
Vincent Buchholz <vbu@miracle.dk>
2023-12-27 12:47:05 +0100

    adds dockerfile

--- next.config.js
+++ next.config.js
@@ -4,12 +4,10 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:8080/api/:path*',
+        destination: 'http://10.109.134.171:8086/api/:path*',
       },
-      // {
-      //   source: '/login/:path*',
-      //   destination: .env.API_URL+':8080/:path*',
-      // },
+
     ];
   },
+  output: 'standalone'
 };
